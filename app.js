657119971d66f270dc7726ae0c6d6f8a2a2151af
AnushSingla <singlaanush18@gmail.com>
2025-02-16 22:42:10 +0530

    good

--- app.js
+++ app.js
@@ -83,13 +83,14 @@ const SessionOptions ={
 
 
 app.use(session(SessionOptions));
-app.use(flash());
+
 
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+app.use(flash());
 
 app.use((req,res,next)=>{
   res.locals.success=req.flash("success")||null;
--- controllers/listing.js
+++ controllers/listing.js
@@ -15,8 +15,7 @@ module.exports.index=async(req,res)=>{
   }
 
   module.exports.renderlisting=async(req,res)=>{
-    currUser=req.user;
-    success=req.flash("success")
+   
     let {id}=req.params;
     const listing=await Listing.findById(id).populate("reviews").populate("owner").populate({path:"reviews",populate:{
       path:"author"
@@ -29,8 +28,7 @@ module.exports.index=async(req,res)=>{
   }
 
   module.exports.rendernewlisting=async(req,res,next)=>{
-    currUser=req.user;
-    success=req.flash("success")
+  
     let url=req.file.path;
     let filename=req.file.filename;
     const newlisting = new Listing(req.body.listing);
@@ -41,7 +39,7 @@ module.exports.index=async(req,res)=>{
 
     await newlisting.save();
     req.flash("success","Listing Added Successfully")
-    res.redirect("/listings",{currUser,success})
+    res.redirect("/listings")
     
     
   }
@@ -53,12 +51,11 @@ module.exports.index=async(req,res)=>{
     const listing=await Listing.findById(id);
     let originalURL=listing.image.url;
     originalURL=originalURL.replace("/upload","/upload/h_300,w_250")
-    res.render("listings/edit.ejs",{listing,originalURL},{currUser,success});
+    res.render("listings/edit.ejs",{listing,originalURL},{currUser},{success});
   }
 
   module.exports.rendereditlisting=async(req,res)=>{
-    currUser=req.user;
-    success=req.flash("success")
+   
     let {id} = req.params;
     let listing=await Listing.findByIdAndUpdate(id,{...req.body.listing})
     if(typeof req.file!=="undefined"){
@@ -69,16 +66,15 @@ module.exports.index=async(req,res)=>{
     }
    
     req.flash("success","Listing Updated Successfully")
-    res.redirect(`/listings/${id}`,{  currUser,success})
+    res.redirect(`/listings/${id}`)
   }
 
   module.exports.destroylisting=async(req,res)=>{
-    currUser=req.user;
-    success=req.flash("success")
+  
       let {id}=req.params;
       const listing=await Listing.findByIdAndDelete(id);
       req.flash("success","Listing Deleted Successfully")
-      res.redirect("/listings",{currUser,success})
+      res.redirect("/listings")
     }
 
 
